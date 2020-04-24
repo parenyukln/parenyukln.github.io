7fdf7538e9f114b38abe4cf3d29867b74cd5af27
leonidmircod <leonid@mircod.com>
2020-04-24 17:41:34 +0300

    test ble app

---
+++ precache-manifest.a01a8686020332e1f90a319cc2ee3e31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86ced2232bc7adce1780daff340277c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb6d6dc466358aac5d8a",
+    "url": "/static/css/main.b7150a5b.chunk.css"
+  },
+  {
+    "revision": "7ea198e7f519651337be",
+    "url": "/static/js/2.78c40fc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.78c40fc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb6d6dc466358aac5d8a",
+    "url": "/static/js/main.833754e6.chunk.js"
+  },
+  {
+    "revision": "f083edf19999e891007b",
+    "url": "/static/js/runtime-main.32c2599b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
